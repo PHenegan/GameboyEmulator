e9b4eb90047c6f60ad7de72c5174a8c2a08bcfbb
Patrick Henegan <phenegan03@gmail.com>
2024-07-13 14:51:20 -0400

    feat: restructure memory controller to match MBCs, test, add gitignore and lock file which should have been in the previous commit

--- libgb-rs/src/memory/mod.rs
+++ libgb-rs/src/memory/mod.rs
@@ -2,14 +2,15 @@ use cartridge::CartridgeMemoryBankController;
 
 use crate::utils::{Merge, Split};
 
-mod cartridge;
+pub mod cartridge;
 
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
 pub struct MemoryWriteError;
 
 pub trait MemoryController {
     fn load_byte(&self, address: u16) -> Option<u8>;
     fn load_half_word(&self, address: u16) -> Option<u16>;
-    fn store_byte(&mut self, address: u16, data: u8) -> Result<(), MemoryWriteError>;  
+    fn store_byte(&mut self, address: u16, data: u8) -> Result<u8, MemoryWriteError>;
     fn store_half_word(&mut self, address: u16, data: u16) -> Result<(), MemoryWriteError>;
 }
 
@@ -32,7 +33,7 @@ pub struct DmgMemoryController {
     cartridge: Box<dyn CartridgeMemoryBankController>,
     ram: [u8; DMG_RAM_SIZE],
     vram: [u8; DMG_VRAM_SIZE],
-    system: [u8; DMG_RES_SIZE]
+    system: [u8; DMG_RES_SIZE],
 }
 
 impl DmgMemoryController {
@@ -41,83 +42,278 @@ impl DmgMemoryController {
             cartridge,
             ram: [0; DMG_VRAM_SIZE],
             vram: [0; DMG_VRAM_SIZE],
-            system: [0; DMG_RES_SIZE]
-        }
-    }
-    fn get_byte(&self, address: u16) -> Option<&u8> {
-        match address {
-            0 ..= DMG_ROM_END => {
-                self.cartridge.get_rom_byte(address)
-            },
-            DMG_EXT_START ..= DMG_EXT_END => {
-                self.cartridge.get_mem_byte(address)
-            },
-            DMG_VRAM_START ..= DMG_VRAM_END => {
-                Some(&self.vram[(address - DMG_VRAM_START) as usize])
-            },
-            DMG_RAM_START ..= DMG_RAM_END => {
-                Some(&self.ram[(address - DMG_RAM_START) as usize])
-            },
-            DMG_RES_START ..= DMG_RES_END => {
-                Some(&self.system[(address - DMG_RES_START) as usize])
-            }
-            _ => None
-        }
-    }
-
-    fn get_byte_mut<'a>(&'a mut self, address: u16) -> Option<&'a mut u8> {
-        match address {
-            0 ..= DMG_ROM_END => {
-                None
-            },
-            DMG_VRAM_START ..= DMG_VRAM_END => {
-                Some(&mut self.vram[(address - DMG_VRAM_START) as usize])
-            },
-            DMG_EXT_START ..= DMG_EXT_END => {
-                self.cartridge.get_mem_byte_mut(address)
-            },
-            DMG_RAM_START ..= DMG_RAM_END => {
-                Some(&mut self.ram[(address - DMG_RAM_START) as usize])
-            },
-            DMG_RES_START ..= DMG_RES_END => {
-                Some(&mut self.system[(address - DMG_RES_START) as usize])
-            }
-            _ => None
+            system: [0; DMG_RES_SIZE],
         }
     }
 }
 
 impl MemoryController for DmgMemoryController {
     fn load_byte(&self, address: u16) -> Option<u8> {
-        self.get_byte(address).copied()
+        match address {
+            0..=DMG_ROM_END => {
+                self.cartridge.read_rom(address)
+            }
+            DMG_EXT_START..=DMG_EXT_END => {
+                self.cartridge.read_mem(address - DMG_EXT_START)
+            }
+            DMG_VRAM_START..=DMG_VRAM_END => {
+                Some(self.vram[(address - DMG_VRAM_START) as usize])
+            }
+            DMG_RAM_START..=DMG_RAM_END => {
+                Some(self.ram[(address - DMG_RAM_START) as usize])
+            }
+            DMG_RES_START..=DMG_RES_END => {
+                Some(self.system[(address - DMG_RES_START) as usize])
+            }
+            _ => None
+        }
     }
 
     fn load_half_word(&self, address: u16) -> Option<u16> {
-        let left = self.get_byte(address)?;
-        let right = self.get_byte(address + 1)?;
+        let left = self.load_byte(address)?;
+        let right = self.load_byte(address + 1)?;
 
-        Some(left.merge(*right))
+        Some(left.merge(right))
     }
 
-    fn store_byte(&mut self, address: u16, data: u8) -> Result<(), MemoryWriteError> {
-        let byte = self.get_byte_mut(address).ok_or(MemoryWriteError)?;
-        *byte = data;
-
-        Ok(())
+    fn store_byte(&mut self, address: u16, data: u8) -> Result<u8, MemoryWriteError> {
+        match address {
+            0..=DMG_ROM_END => {
+                self.cartridge.write_rom(address, data)
+                    .map(|_| data)
+            }
+            DMG_VRAM_START..=DMG_VRAM_END => {
+                let address = (address - DMG_VRAM_START) as usize;
+                let prev = self.vram[address];
+                self.vram[address] = data;
+                Ok(prev)
+            }
+            DMG_EXT_START..=DMG_EXT_END => {
+                self.cartridge.write_mem(address - DMG_EXT_START, data)
+            }
+            DMG_RAM_START..=DMG_RAM_END => {
+                let address = (address - DMG_RAM_START) as usize;
+                let prev = self.vram[address];
+                self.ram[address] = data;
+                Ok(prev)
+            }
+            DMG_RES_START..=DMG_RES_END => {
+                let address = (address - DMG_RES_START) as usize;
+                let prev = self.vram[address];
+                self.system[address] = data;
+                Ok(prev)
+            }
+            _ => Err(MemoryWriteError)
+        }
     }
 
     fn store_half_word(&mut self, address: u16, data: u16) -> Result<(), MemoryWriteError> {
         let (left_data, right_data) = data.split();
-        
-        // Avoid using store_byte so that the function will not load the first half when the second 
-        // half of the data cannot be loaded
-        let left = self.get_byte_mut(address)
-            .ok_or(MemoryWriteError)?;
-        *left = left_data;
-        let right = self.get_byte_mut(address)
-            .ok_or(MemoryWriteError)?;
-        *right = right_data;
 
+        let prev_left = self.store_byte(address, left_data)?;
+        let right = self.store_byte(address + 1, right_data);
+        if right.is_err() {
+            self.store_byte(address, prev_left).unwrap();
+            return Err(MemoryWriteError);
+        }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use mockall::predicate::eq;
+    use crate::memory::cartridge::MockCartridgeMemoryBankController;
+    use super::*;
+
+    #[test]
+    fn test_rom_write_fails() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_write_rom()
+            .return_const(Err(MemoryWriteError));
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.store_byte(42, 42);
+
+        assert!(result.is_err(), "Test that writing to a rom does not work");
+    }
+
+    #[test]
+    fn test_rom_read() {
+        // NOTE - I couldn't figure out how to put in a mock Option<&u8>
+        // into this without doing some jank static lifetime stuff so I'm having it
+        // return None and checking that the address gets passed correctly
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_read_rom()
+            .times(1)
+            .with(eq(42))
+            .return_const(Some(210));
+        let controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.load_byte(42);
+
+        assert_eq!(result, Some(210), "Test reading from a ROM address");
+    }
+
+    #[test]
+    fn test_vram_io() {
+        let mock = MockCartridgeMemoryBankController::new();
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        assert_eq!(controller.load_byte(0x8000), Some(0));
+
+        let result = controller.store_byte(0x8000, 80);
+
+        assert_eq!(result, Ok(0), "Test writing to VRAM");
+        assert_eq!(controller.load_byte(0x8000), Some(80), "Test changed RAM value");
+    }
+
+    #[test]
+    fn test_cart_ram_read_success() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_read_mem()
+            .with(eq(42))
+            .return_const(Some(0x22));
+        let controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.load_byte(DMG_EXT_START + 42);
+
+        assert_eq!(result, Some(0x22), "Test reading from cartridge RAM");
+    }
+
+    #[test]
+    fn test_cart_ram_read_fail() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_read_mem()
+            .with(eq(42))
+            .return_const(None);
+        let controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.load_byte(DMG_EXT_START + 42);
+
+        assert!(result.is_none(), "Test reading cartridge RAM when it doesn't exist");
+    }
+
+    #[test]
+    fn test_cart_ram_write_fail() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_write_mem()
+            .with(eq(42), eq(42))
+            .return_const(Err(MemoryWriteError));
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.store_byte(DMG_EXT_START + 42, 42);
+
+        assert!(result.is_err(), "Test that accessing non-existent cartridge RAM errors");
+    }
+
+    #[test]
+    fn test_ram_io() {
+        let mock = MockCartridgeMemoryBankController::new();
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        assert_eq!(controller.load_byte(0xC042), Some(0));
+
+        let result = controller.store_byte(0xC042, 28);
+
+        assert_eq!(result, Ok(0), "Test writing to system RAM");
+        assert_eq!(controller.load_byte(0xC042), Some(28), "Test changed RAM value");
+    }
+
+    #[test]
+    fn test_reserved_io() {
+        let mock = MockCartridgeMemoryBankController::new();
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        assert_eq!(controller.load_byte(0xFE42), Some(0));
+
+        let result = controller.store_byte(0xFE42, 7);
+
+        assert_eq!(result, Ok(0), "Test writing to reserved addresses");
+        assert_eq!(controller.load_byte(0xFE42), Some(7), "Test changed RAM value");
+    }
+
+    #[test]
+    fn test_load_half_word_valid_address() {
+        let mock = MockCartridgeMemoryBankController::new();
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+        controller.store_byte(DMG_RAM_START, 0x04).unwrap();
+        controller.store_byte(DMG_RAM_START + 1, 0x28).unwrap();
+
+        let result = controller.load_half_word(DMG_RAM_START);
+
+        assert_eq!(result, Some(0x0428), "Test valid 16-bit load");
+    }
+
+    #[test]
+    fn test_load_half_word_invalid_first_byte() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_read_mem()
+            .with(eq(0x1FFF))
+            .return_const(None);
+        let controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.load_half_word(0xBFFF);
+
+        assert!(result.is_none(), "Test loading address where 1st byte is an invalid address")
+    }
+
+    #[test]
+    fn test_load_half_word_invalid_second_byte() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_read_mem()
+            .with(eq(0))
+            .return_const(None);
+        let controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.load_half_word(0x9FFF);
+
+        assert!(result.is_none(), "Test loading address where 2nd byte is an invalid address");
+    }
+
+    #[test]
+    fn test_store_half_word_valid_address() {
+        let mock = MockCartridgeMemoryBankController::new();
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.store_half_word(DMG_RAM_START, 0x0428);
+
+        assert_eq!(result, Ok(()), "Test storing 2 bytes into a valid address");
+        assert_eq!(controller.load_byte(DMG_RAM_START), Some(0x04), "Test first loaded byte");
+        assert_eq!(controller.load_byte(DMG_RAM_START + 1), Some(0x28), "Test second loaded byte");
+    }
+
+    #[test]
+    fn test_store_half_byte_invalid_first_byte() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_write_rom()
+            .with(eq(DMG_ROM_END), eq(0x08))
+            .return_const(Err(MemoryWriteError));
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.store_half_word(DMG_ROM_END, 0x0812);
+
+        assert!(result.is_err(), "Test that the invalid write failed");
+        assert_eq!(
+            controller.load_byte(DMG_VRAM_START), Some(0),
+            "Test that the valid address is unchanged"
+        );
+    }
+
+    #[test]
+    fn test_store_half_byte_invalid_second_byte() {
+        let mut mock = MockCartridgeMemoryBankController::new();
+        mock.expect_write_mem()
+            .with(eq(0), eq(0x06))
+            .return_const(Err(MemoryWriteError));
+        let mut controller = DmgMemoryController::new(Box::new(mock));
+
+        let result = controller.store_half_word(DMG_VRAM_END, 0x0106);
+
+        assert_eq!(result, Err(MemoryWriteError), "Test that the invalid write failed");
+        assert_eq!(
+            controller.load_byte(DMG_VRAM_END), Some(0),
+            "Test that the valid address is unchanged"
+        );
     }
 }
