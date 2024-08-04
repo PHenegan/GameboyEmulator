c5a7789f3a285f51409ac8c1db220ecf4b395908
Patrick Henegan <phenegan03@gmail.com>
2024-08-04 18:30:23 -0400

    feat(mbc2): implement MBC2 memory writing

--- libgb-rs/src/memory/cartridge/mbc2.rs
+++ libgb-rs/src/memory/cartridge/mbc2.rs
@@ -22,15 +22,29 @@ impl CartridgeMapper for MBC2 {
     }
 
     fn read_mem(&self, address: u16) -> Option<u8> {
-        let address = address % 0x200;
+        // only use the first 9 bits since there are only 512 entries in memory
+        let address = (address & 0x1FF) as usize;
         if self.ram_enabled {
-            self.ram.get(address as usize).copied()
+            self.ram.get(address).copied()
         } else {
             Some(0xFF)
         }
     }
 
     fn write_mem(&mut self, address: u16, data: u8) -> Result<u8, MemoryWriteError> {
-        todo!()
+        if !self.ram_enabled {
+            return Ok(0xFF)
+        }
+        // only use the first 9 bits since there are only 512 entries in memory
+        let address = (address & 0x1FF) as usize;
+        let half_byte = self.ram.get_mut(address)
+            .ok_or(MemoryWriteError)?;
+        let old_value = *half_byte;
+
+        // only use the lower 4 bits of the address, leaving the rest as 0
+        // (technically the behavior is undefined for actual MBC2 cartridges)
+        *half_byte = data & 0xF;
+
+        Ok(old_value)
     }
 }
