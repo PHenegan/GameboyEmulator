5bd1d8092fe4b2970088014891fe175073f6173d
Patrick Henegan <phenegan03@gmail.com>
2024-08-15 21:04:57 -0400

    feat(mbc3): Implement ROM writing (with a stub for the RTC portion)

--- libgb-rs/src/memory/cartridge/mbc3.rs
+++ libgb-rs/src/memory/cartridge/mbc3.rs
@@ -1,6 +1,11 @@
 use crate::memory::cartridge::{CartridgeMapper, MemBank, ROM_BANK_SIZE, RomBank};
 use crate::memory::MemoryWriteError;
 
+const SET_ROM_BANK_START: usize = 0x2000;
+const SET_RAM_BANK_START: usize = 0x4000;
+const LATCH_CLOCK_START: usize = 0x6000;
+const ROM_END: usize = 0x8000;
+
 pub struct MBC3 {
     rom: Vec<RomBank>,
     ram: Vec<MemBank>,
@@ -26,7 +31,25 @@ impl CartridgeMapper for MBC3 {
     }
 
     fn write_rom(&mut self, address: u16, data: u8) -> Result<(), MemoryWriteError> {
-        todo!()
+        let address = address as usize;
+        match address {
+            0..SET_ROM_BANK_START => {
+                self.ram_enabled = data == 0xA0;
+                Ok(())
+            }
+            SET_ROM_BANK_START..SET_RAM_BANK_START => {
+                self.rom_bank = data & 0x7F;
+                Ok(())
+            }
+            SET_RAM_BANK_START..LATCH_CLOCK_START {
+                self.ram_bank = data & 0x0F;
+                Ok(())
+            }
+            LATCH_CLOCK_START..ROM_END => {
+                todo!()
+            }
+            _ => Err(MemoryWriteError)
+        }
     }
 
     fn read_mem(&self, address: u16) -> Option<u8> {
