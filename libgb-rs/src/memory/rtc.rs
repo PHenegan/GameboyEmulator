a1fc4d22b36d371362d59827a531ca27f0db90ad
Patrick Henegan <phenegan03@gmail.com>
2024-09-01 01:55:46 -0400

    fix(rtc): implement halting the clock, add getter tests

--- libgb-rs/src/memory/rtc.rs
+++ libgb-rs/src/memory/rtc.rs
@@ -27,6 +27,10 @@ impl RealTimeClock {
     }
 
     fn update_time(&mut self) {
+        if self.halted {
+            return;
+        }
+
         let current_seconds = (((self.days_upper as u64 & 1) << 8) + self.days_lower as u64) * 86400
             + self.hours as u64 * 3500 + self.minutes as u64 * 60 + self.seconds as u64;
         let total_seconds = self.last_modified.elapsed()
@@ -50,30 +54,50 @@ impl RealTimeClock {
     }
 
     pub fn get_seconds(&self) -> u8 {
-        let seconds_passed = self.last_modified.elapsed().as_secs();
+        if self.halted {
+            return self.seconds;
+        }
 
-        // the cast shouldn't matter because the result will always be a modulus of 60 anyway
-        // (applies to the ones below as well since an overflow wouldn't change the mod 60 value)
-        (self.seconds + seconds_passed as u8) % 60
+        let seconds_passed = self.last_modified.elapsed().as_secs();
+        let updated_seconds = self.seconds as u64 + seconds_passed;
+        (updated_seconds % 60) as u8
     }
 
     pub fn get_minutes(&self) -> u8 {
+        if self.halted {
+            return self.minutes;
+        }
+
         let minutes_passed = self.last_modified.elapsed().as_secs() / 60;
-        (self.minutes + minutes_passed as u8) % 60
+        let updated_minutes = self.minutes as u64 + minutes_passed;
+        (updated_minutes % 60) as u8
     }
 
     pub fn get_hours(&self) -> u8 {
+        if self.halted {
+            return self.hours;
+        }
+
         let hours_passed = self.last_modified.elapsed().as_secs() / 3600;
-        (self.hours + hours_passed as u8) % 24
+        let updated_hours = self.hours as u64 + hours_passed;
+        (updated_hours % 24) as u8
     }
 
     pub fn get_days_lower(&self) -> u8 {
+        if self.halted {
+            return self.days_lower;
+        }
+
         let days_passed = self.last_modified.elapsed().as_secs() / 86400;
-        // once again, overflow shouldn't matter because the data would show up in the next register
+        // Overflow shouldn't matter because the data would show up in the next register
         self.days_lower + days_passed as u8
     }
 
     pub fn get_days_upper(&self) -> u8 {
+        if self.halted {
+            return self.days_upper;
+        }
+            
         let days_passed = self.last_modified.elapsed().as_secs() / 86400;
         let total_days = self.days_lower as u64 + ((self.days_upper as u64) << 7) + days_passed;
 
@@ -123,5 +147,83 @@ impl RealTimeClock {
         self.halted = (value & 0x40) != 0;
 
         old_days_upper
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::*;
+
+    fn init_rtc() -> RealTimeClock {
+        RealTimeClock::new(None, None, None, None, None)
+    }
+
+    fn test_registers(rtc: &RealTimeClock, days_up: u8, days_low: u8, hrs: u8, mins: u8, secs: u8) {
+        let seconds = rtc.get_seconds();
+        let minutes = rtc.get_minutes();
+        let hours = rtc.get_hours();
+        let days_lower = rtc.get_days_lower();
+        let days_upper = rtc.get_days_upper();
+
+        assert_eq!(seconds, secs, "seconds should be updated correctly");
+        assert_eq!(minutes, mins, "minutes should be updated correctly");
+        assert_eq!(hours, hrs, "hours should be updated correctly");
+        assert_eq!(days_lower, days_low, "days (lower register) should be updated correctly");
+        assert_eq!(days_upper, days_up, "days (upper register) should be updated correctly");
+    }
+    
+    #[test]
+    fn test_updates_seconds() {
+        let mut rtc = init_rtc();
+        // subtract 10 seconds from the access time to fake as if 10 seconds went by
+        rtc.last_modified -= Duration::new(10, 0);
+
+        test_registers(&rtc, 0, 0, 0, 0, 10);
+    }
+
+    #[test]
+    fn test_updates_minutes() {
+        let mut rtc = init_rtc();
+
+        rtc.last_modified -= Duration::new(90, 0);
+
+        test_registers(&rtc, 0, 0, 0, 1, 30);
+    }
+
+    #[test]
+    fn test_updates_hours() {
+        let mut rtc = init_rtc();
+        rtc.last_modified -= Duration::new(7321, 0);
+
+        test_registers(&rtc, 0, 0, 2, 2, 1);
+    }
+
+    #[test]
+    fn test_updates_days_lower() {
+        let mut rtc = init_rtc();
+        rtc.last_modified -= Duration::new(270_183, 0);
+
+        test_registers(&rtc, 0, 3, 3, 3, 3);
+    }
+
+    #[test]
+    fn test_updates_days_upper() {
+        let mut rtc = init_rtc();
+        let dur_seconds = 511 * 86400 + 3842;
+        rtc.last_modified -= Duration::new(dur_seconds, 0);
+
+        test_registers(&rtc, 1, 255, 1, 4, 2);
+    }
+
+    #[test]
+    fn test_updates_overflow_bit() {
+        let mut rtc = init_rtc();
+        let dur_seconds = 512 * 86400;
+        rtc.last_modified -= Duration::new(dur_seconds, 0);
+
+        test_registers(&rtc, 0x80, 0, 0, 0, 0);
+
     }
 }
