d16524d318bc410eb73037b47240a31cb0d471c5
Patrick Henegan <phenegan03@gmail.com>
2024-10-02 20:23:34 -0400

    fix(cpu, mem): rewrite bad CPU register code, update logic to be little-endian

--- libgb-rs/src/cpu.rs
+++ libgb-rs/src/cpu.rs
@@ -1,6 +1,8 @@
 use crate::utils::{Merge, Split};
 
-#[derive(Clone, Copy)]
+/// # CpuRegister
+/// An enum storing each of the lettered registers in a Game Boy CPU.
+#[derive(Debug, Clone, Copy)]
 pub enum CpuRegister {
     A = 0,
     B = 1,
@@ -12,7 +14,38 @@ pub enum CpuRegister {
     F = 7
 }
 
-/// The CPU of a Gameboy/Gameboy Color system
+/// #FlagRegister
+/// A convenient struct for holding CPU flags
+#[derive(Debug, Clone, Copy)]
+pub struct FlagRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool
+}
+
+impl From<FlagRegister> for u8 {
+    fn from(value: FlagRegister) -> Self {
+        ((value.zero as u8) << 7)
+            | ((value.subtract as u8) << 6)
+            | ((value.half_carry as u8) << 5)
+            | ((value.carry as u8) << 4)
+    }
+}
+
+impl From<u8> for FlagRegister {
+    fn from(value: u8) -> Self {
+        FlagRegister {
+            zero: (value & 0x80) != 0,
+            subtract: (value & 0x40) != 0,
+            half_carry: (value & 0x20) != 0,
+            carry: (value & 0x10) != 0
+        }
+    }
+}
+
+/// # CpuData
+/// The CPU Registers of a Gameboy/Gameboy Color system
 pub struct CpuData {
     // 7 8-bit registers A-L, followed by the last flag register F 
     registers: Vec<u8>, 
@@ -21,40 +54,59 @@ pub struct CpuData {
 }
 
 impl CpuData {
-    pub fn new() -> CpuData {
-        return CpuData {
-            registers: vec![0, 0, 0, 0, 0, 0, 0, 0],
+    pub fn new() -> Self {
+        CpuData {
+            registers: vec![0; 8],
             sp: 0,
             pc: 0
-        };
+        }
     }
 
-    pub fn get_register<'a>(&'a self, idx: &CpuRegister) -> &'a u8 {
-        self.registers.get(*idx as usize)
-            .unwrap()
+    pub fn get_register(&self, idx: CpuRegister) -> u8 {
+        // option isn't necessary since the type is being used to guarantee bounds
+        self.registers[idx as usize]
     }
 
-    pub fn get_register_mut<'a>(&'a mut self, idx: &CpuRegister) -> &'a mut u8 {
-       self.registers.get_mut(*idx as usize)
-           .unwrap()
+    pub fn set_register(&mut self, idx: CpuRegister, value: u8) {
+        // result isn't necessary since the type is being used to guarantee bounds
+        self.registers[idx as usize] = value;
     }
 
-    pub fn get_joined_registers(&self, idx1: &CpuRegister, idx2: &CpuRegister) -> u16 {
-        let left = self.get_register(idx1);
-        let right = self.get_register(idx2);
-        left.merge(*right)
+    /// Get a 16-bit by joining two bytes from the given registers in Little-Endian ordering
+    pub fn get_joined_registers(&self, idx1: CpuRegister, idx2: CpuRegister) -> u16 {
+        let right = self.get_register(idx1);
+        let left = self.get_register(idx2);
+        left.merge(right)
     }
 
-    pub fn set_joined_registers(&mut self, idx1: &CpuRegister, idx2: &CpuRegister, data: u16) {
+    /// Store a 16-bit value by splitting the given data and storing it in Little-Endian ordering
+    /// into the given registers
+    pub fn set_joined_registers(&mut self, idx1: CpuRegister, idx2: CpuRegister, data: u16) {
         let (left_data, right_data) = data.split();
 
         // Register 1 gets the 8 most significant bits
-        let reg1: &mut u8 = self.get_register_mut(idx1);
-        *reg1 = left_data; 
-
+        self.set_register(idx1, right_data);
         // Register 2 gets the 8 least significant bits
-        let reg2: &mut u8 = self.get_register_mut(idx2);
-        *reg2 = right_data;
+        self.set_register(idx2, left_data);
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::utils::Merge;
+
+    use super::{CpuData, CpuRegister};
+
+    #[test]
+    fn test_endianness() {
+        let mut data = CpuData::new();
+        data.set_joined_registers(CpuRegister::B, CpuRegister::C, 0xBEEF);
+
+        let right = data.get_register(CpuRegister::B);
+        let left = data.get_register(CpuRegister::C);
+
+        let n16 = data.get_joined_registers(CpuRegister::B, CpuRegister::C);
+
+        assert_eq!(n16, left.merge(right), "Data should be assigned in Little Endian order");
+    }
+}
