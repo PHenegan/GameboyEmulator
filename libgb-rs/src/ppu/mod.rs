396de8a5d01bf048c7310eddcc9351aa06ce5ff9
Patrick Henegan <phenegan03@gmail.com>
2024-12-29 19:54:09 -0500

    feat(ppu): Add docs for tile formatting

--- libgb-rs/src/ppu/mod.rs
+++ libgb-rs/src/ppu/mod.rs
@@ -6,18 +6,28 @@ trait Ppu {
 }
 
 /// # Tile
-/// Represents a Tile (8x8 square of pixels in the PPU's render frame
+/// Represents a Tile (8x8 square of pixels) to be displayed on a Game Boy screen
+///
+/// Parameters:
+/// - `rows`: the block of 8 pixel rows in each tile. Each row is 8 pixels wide.
+///           the leftmost byte of each row contains the second bit in each pixel's color,
+///           while the first byte contains the first bit. This is because of the little-endian
+///           byte ordering.
 struct Tile {
-    x: u8,
+    pub rows: [u16; 8],
+    x: u8, // these might be unnecessary but I'm putting them here for now
     y: u8,
-    // TODO - finish
     // (Figure out how to make this in a way that doesn't get messed up with Gameboy Color
     // support)
+    // Q: Should Sprites be part of this struct? They can be 8x16 and I'm guessing they follow
+    //    the same format in terms of bit layout?
 }
 
 /// # DmgPpu
 /// Represents the Pixel Processing Unit of an original Gameboy system (i.e. non-color)
 struct DmgPpu {
+    tilemap: Vec<Vec<Tile>>,
+    // objects - TODO
     // TODO - finish
 }
 
