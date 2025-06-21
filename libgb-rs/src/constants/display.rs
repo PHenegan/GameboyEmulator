45634a4a03bd7d88cc540e1b7bfc3fe17b61ca81
Patrick Henegan <phenegan03@gmail.com>
2025-06-20 20:03:50 -0400

    wip(ppu): load tiles, sprites, and maps

--- libgb-rs/src/constants/display.rs
+++ libgb-rs/src/constants/display.rs
@@ -1,11 +1,20 @@
-// Blocks of memory where Tile maps are stored
+pub const TILE_SIZE: usize = 16;
+pub const TILE_DATA_SIZE: usize = 256;
+pub const TILE_MAP_SIZE: usize = 32;
+
+// Blocks of memory where Tile data is stored
 pub const TILE_BLOCK_0: u16 = 0x8000;
 pub const TILE_BLOCK_1: u16 = 0x8800;
 pub const TILE_BLOCK_2: u16 = 0x9000;
 
+// Blocks of memory where tile maps can be stored for the window or background
+pub const TILE_MAP_0: u16 = 0x9800;
+pub const TILE_MAP_1: u16 = 0x9BFF;
+
 // OAM region where sprite data is stored
 pub const OAM_START: u16 = 0xFE00;
 pub const OAM_END: u16 = 0xFE9F;
+pub const OBJ_SIZE: usize = 40;
 
 // LCD registers
 pub const LCDC: u16 = 0xFF40; // LCD control
@@ -13,10 +22,11 @@ pub const LY: u16 = 0xFF44; // LCD Y coordinate
 pub const LYC: u16 = 0xFF45; // LY Compare
 pub const STAT: u16 = 0xFF41; // LCD Status
 
-// Scrolling position
+// Offsets of the background on the *canvas* (larger than the screen)
+// "Scroll X" and "Scroll Y"
 pub const SCY: u16 = 0xFF42;
 pub const SCX: u16 = 0xFF43;
 
-// Window position
+// Window position on the screen
 pub const WY: u16 = 0xFF4A;
 pub const WX: u16 = 0xFF4B;
--- libgb-rs/src/ppu/mod.rs
+++ libgb-rs/src/ppu/mod.rs
@@ -1,4 +1,7 @@
-use crate::{constants::display::{TILE_BLOCK_0, TILE_BLOCK_1, TILE_BLOCK_2}, GameBoySystem};
+use std::array::from_fn;
+
+use crate::GameBoySystem;
+use crate::constants::display;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct PpuError;
@@ -28,32 +31,40 @@ pub struct PpuError;
 /// - there can only be 10 sprites on a given line at any point in time
 ///     - any more, and their data should be skipped *on that line*
 ///     - decided by the 11th item in the list of sprites - order determined by the program
-///
-/// ## Fields:
-/// - `x`: position on the x-axis of the screen to render the sprite
-/// - `y`: position on the y-axis of the screen to render the sprite
-/// - `tile_num`: the index of the sprite in the tilesheet which contains its image data
-/// - `priority`: whether or not the sprite should be drawn on top of the background.
-///               If false, the sprite will only be drawn on top of colors with a pallete ID of 0
+#[derive(Default)]
 pub struct Sprite {
+    /// position on the x-axis of the screen to render the sprite
     x: u8,
+    /// position on the y-axis of the screen to render the sprite
     y: u8,
+    /// the index of the sprite in the tilesheet which contains its image data
     tile_num: u8,
-    // if priority is 0, only draw the sprite on top of background pixels w/ pallette id 0
-    // otherwise, draw on top of all background
+    /// whether or not the sprite should be drawn on top of the background.
+    /// If false, the sprite will only be drawn on top of colors with a pallete ID of 0
     priority: bool,
-    flip_x: bool,
-    flip_y: bool,
-    pallete: u8, // takes up 5 bits?
+    /// whether the sprite should be horizontally flipped
+    x_flip: bool,
+    /// whether the sprite should be vertically flipped
+    y_flip: bool,
+    /// whether to use the palette specified by OBP0 or OBP1 in non-CGB mode
+    palette_dmg: bool,
+    // These are commented out because they are specific to the Game Boy Color
+    // palette_cgb: u8,
+    // bank: u8,
 }
 
-/// # Window
-/// Represents the "Window" tiles used by the Game Boy's PPU. These can be drawn on top of the
-/// background (and sprites?) as a separate map. This is primarily used for menus, HUDs, etc.
-pub struct Window {
-    x: u8,
-    y: u8,
-    address: u16,
+impl From<[u8; 4]> for Sprite {
+    fn from(bytes: [u8; 4]) -> Self {
+        Sprite {
+            x: bytes[0],
+            y: bytes[1],
+            tile_num: bytes[2],
+            priority: bytes[3] & 0x80 != 0,
+            x_flip: bytes[3] & 0x20 != 0,
+            y_flip: bytes[3] & 0x40 != 0,
+            palette_dmg: bytes[3] & 0x8 != 0,
+        }
+    }
 }
 
 pub struct DmgPpu {
@@ -63,54 +74,156 @@ pub struct DmgPpu {
     scroll_y: u8,
     signed_tile_index: bool,
     object_map: Vec<Tile>,
+    lcd_control: LcdFlags,
+}
+
+/// # LcdFlags
+/// A convenience struct to separate the LCD control register into its separate flags
+struct LcdFlags {
+    /// sets whether the LCD is receiving power 
+    enabled: bool,
+    /// if true use 0x9C00-0x9FFF for window tile map, otherwise use 0x9800-0x9BFF
+    window_map_high: bool,
+    /// sets whether the window map should be drawn over the screen
+    window_enabled: bool,
+    tile_data_high: bool,
+    // If true use 0x9C00-0x9FFF for background map, otherwise use 0x9800-0x9BFF
+    background_map_high: bool,
+    /// use 8x16 sprites if true, 8x8 sprites otherwise
+    tall_sprites: bool,
+    /// sets whether sprites should be drawn
+    enable_sprites: bool,
+    /// If in CGB mode (game boy color), setting this bit will draw all objects on top of
+    /// the background and window. If in DMG mode, the background and window will not be displayed
+    priority: bool,
+}
+
+impl From<u8> for LcdFlags {
+    fn from(value: u8) -> Self {
+        LcdFlags {
+            enabled: value & 0x80 != 0,
+            window_map_high: value & 0x40 != 0,
+            window_enabled: value & 0x20 != 0,
+            tile_data_high: value & 0x10 != 0,
+            background_map_high: value & 0x8 != 0,
+            tall_sprites: value & 0x4 != 0,
+            enable_sprites: value & 0x2 != 0,
+            priority: value & 0x1 != 0,
+        }
+    }
 }
 
 /// # Tile
 /// represents a single 8x8 block of pixels on the Game Boy's screen
-type Tile = [u8; 16];
+type Tile = [u8; display::TILE_SIZE];
+
+/// # Tile
+/// represents a mapping of a position on the screen to the index of a tile in the tile data. 
+type TileMap = [[u8; display::TILE_MAP_SIZE]; display::TILE_MAP_SIZE];
+
+pub trait GameBoyPpu {
+
+}
 
 impl GameBoySystem {
-
     /// # ppu_draw
     /// A single step in the Game Boy's PPU (Pixel Processing Unit) drawing pipeline
     pub fn ppu_draw(&mut self) -> Result<(), PpuError> {
-        let sprite_data = self.sprite_map()?;
-        todo!()
+        let sprites = self.sprite_data()?;
+        let sprite_map = self.sprite_maps()?;
+
+        let lcd_control: LcdFlags = self.memory.load_byte(display::LCDC)
+            .ok_or(PpuError)?
+            .into();
+        let tile_data = self.tile_data(lcd_control.tile_data_high)?;
+        let background_map = self.tile_map(lcd_control.background_map_high)?;
+        let window_map = self.tile_map(lcd_control.window_map_high)?;
+        todo!("Finish the pixel pipeline");
     }
 
-    /// # sprites
-    /// Create a list of sprites from the Game Boy's OAM memory
-    pub fn sprites(&self) -> Result<Vec<Sprite>, PpuError> {
+    /// # sprite_maps
+    /// Create a list of sprite maps from the Game Boy's OAM memory
+    pub fn sprite_maps(&self) -> Result<[Sprite; display::OBJ_SIZE], PpuError> {
 
-        todo!()
+        // using default here because the load_byte function might return an error
+        let mut sprites: [Sprite; display::OBJ_SIZE] = from_fn(|_idx| Sprite::default());
+        for idx in 0..display::OBJ_SIZE {
+            // Load the sprite data from the 4 bytes in OAM memory
+            let address = display::OAM_START + (4 * idx) as u16;
+            let mut entry_bytes: [u8; 4] = [0; 4];
+            for byte_idx in 0..4 {
+                entry_bytes[0] = self.memory.load_byte(address + byte_idx)
+                    .ok_or(PpuError)?;
+            };
+            sprites[idx] = Sprite::from(entry_bytes);
+        }
+
+        return Ok(sprites);
     }
 
-    /// # sprite_map
-    /// load the list of tiles from the PPU's memory block
-    pub fn sprite_map(&self) -> Result<[Tile; 256], PpuError> {
-        self.tilemap(false)
+    /// # tile_map
+    /// Create a map storing a set of indices of tiles to draw at specific points on the map
+    ///
+    /// ## Parameters:
+    /// - `high_map`: whether to use memory region 0x9800-0x9BFF (true) or 0x9C00-0x9FFF (false)
+    ///
+    /// ## Returns:
+    /// return an 32 x 32 grid of tile indices, or a PpuError if the tiles cannot be loaded
+    pub fn tile_map(&self, high_map: bool) -> Result<TileMap, PpuError> {
+        let mut tile_map = [[0; display::TILE_MAP_SIZE]; display::TILE_MAP_SIZE];
+        let start = if high_map {
+            display::TILE_MAP_1
+        } else {
+            display::TILE_MAP_0
+        };
+
+        for y in 0..display::TILE_MAP_SIZE {
+            for x in 0..display::TILE_MAP_SIZE {
+                let address = start + (y * display::TILE_MAP_SIZE + x) as u16;
+                tile_map[y][x] = self.memory.load_byte(address)
+                    .ok_or(PpuError)?;
+            }
+        }
+
+        return Ok(tile_map);
     }
 
-    pub fn tilemap(&self, signed_index: bool) -> Result<[Tile; 256], PpuError> {
-        let mut tiles = [[0; 16]; 256];
+    /// # sprite_data
+    /// load the list of sprite tiles from the PPU's memory block
+    pub fn sprite_data(&self) -> Result<[Tile; display::TILE_DATA_SIZE], PpuError> {
+        self.tile_data(false)
+    }
+
+    /// # tile_data
+    /// load a list of tiles from the PPU's memory block
+    /// 
+    /// ## Parameters:
+    /// - `signed_index`: whether or not to treat the tile data as a signed index.
+    ///                   This will load from different memory regions depending on the value
+    /// 
+    /// ## Returns:
+    /// The function returns an array of 256 "tiles," which themselves are 16 bytes of encoded
+    /// palette data to create an 8x8 tile
+    pub fn tile_data(&self, signed_index: bool) -> Result<[Tile; 256], PpuError> {
+        let mut tiles = [[0; display::TILE_SIZE]; display::TILE_DATA_SIZE];
         
         // load the data by halves in order to avoid using signed logic for indices
         let (first, second) = if signed_index {
-            (TILE_BLOCK_2, TILE_BLOCK_1)
+            (display::TILE_BLOCK_2, display::TILE_BLOCK_1)
         } else {
-            (TILE_BLOCK_0, TILE_BLOCK_1)
+            (display::TILE_BLOCK_0, display::TILE_BLOCK_1)
         };
 
         self.load_tile_block(&mut tiles, first, 0)?;
-        self.load_tile_block(&mut tiles, second, 128)?;
+        self.load_tile_block(&mut tiles, second, display::TILE_DATA_SIZE/2)?;
         Ok(tiles)
     }
 
     fn load_tile_block(
-        &self, tiles: &mut [Tile; 256], block_address: u16, idx: usize
+        &self, tiles: &mut [Tile; display::TILE_DATA_SIZE], block_address: u16, idx: usize
     ) -> Result<(), PpuError> {
-        for block_idx in 0..128 {
-            for tile_idx in 0..16 {
+        for block_idx in 0..display::TILE_DATA_SIZE/2 {
+            for tile_idx in 0..display::TILE_SIZE {
                 let address = block_address + (block_idx as u16) * 16 + (tile_idx as u16);
                 tiles[block_idx + idx][tile_idx] = self.memory.load_byte(address)
                     .ok_or(PpuError)?;
